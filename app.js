56d63e94b004cf604d283d8899c02531a1f01776
erlistv <erlistv@gmail.com>
2024-03-02 20:06:05 -0500

    made the main page and default vote buttons

---
+++ app.js
@@ -0,0 +1,14 @@
+let votesUp = 0
+
+let votesDown = 0
+
+
+function voteUp() {
+    votesUp = votesUp + 1
+    document.getElementById("votesupdisplay").innerHTML = votesUp
+}
+
+function voteDown() {
+    votesDown = votesDown + 1
+    document.getElementById("votesdowndisplay").innerHTML = votesDown
+}
